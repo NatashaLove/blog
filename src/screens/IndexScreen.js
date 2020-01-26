f770e0991db8a0dcdf046d465c1ba1a6ef5d0914
Natasha S Love <debora@inbox.ru>
2020-01-25 20:19:44 -0600

    added button
    corrected the code onPress func - creates a new post with it's number on every click of the button

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';//imported hook into the child : useContext - to make use of that contex (from BlogContext)
 //function that's going look at some context object and give us access (to value prop - for example)
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 //need flatlist for  arr of posts (objects)
 import BlogContext from '../context/BlogContext';
 
@@ -8,15 +8,20 @@ import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
 //variable 'blogPosts' here is going to be exactly = to the value prop assigned in the blogcontext.provider
-const blogPosts = useContext(BlogContext);
+//instead of var 'blogPosts' - insert the value - destructured object from BlogContext with callback func:
+const {data, addBlogPost} = useContext(BlogContext);
 
     return (
         <View>
             <Text>Index Screen</Text>
+            <Button title="Add Post" 
+            //callback func onPress ={() => addBlogPost()} can be shortened - rather than creating this extra function -
+            // we can instead just pass a reference to the function that we want to be called anytime a user presses:
+            onPress ={addBlogPost}/> 
              <FlatList
 //data prop =blog posts: our array of objects 
-
-                data={blogPosts}
+//replace 'blogPosts' with 'data' from the object BlogContext - above
+                data={data} // pass in that data property to our flat list
 
 // key extractor: a function that's going to be called with every object inside of our array.
 // from key extractor we have to return a String to be used as a key: in our case we have a unique string -
