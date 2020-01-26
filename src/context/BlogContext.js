31ffd44cde06df8f89e05e97bdffdc7cabad80a7
Natasha S Love <debora@inbox.ru>
2020-01-25 18:19:00 -0600

    created blogPosts arr of objects
    inserted flatlist into the screen to show the list/arr of objects/posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -8,10 +8,26 @@ const BlogContext = React.createContext();
 //into BlogContext - and then exporting Blog context as default 
 export const BlogProvider = ( {children})=> {
 // created component which can accept another component as {children}- argument    
-return <BlogContext.Provider>
+
+//creating arr of blog posts - later will use it as 'value' n blogCobtext.provider:
+const blogPosts = [
+    {title: 'Blog Post #1'},
+    {title: 'Blog Post #2'}
+];
+
+//.Provider accepts information - so must be with the context obj;
+//it's a source of information -and the information we provide it with -
+//is going to be available to all of our child components. (not having to pass it through all screens down)
+//to share some information from the very top level component down to a nested child-
+//- in blogcontext.provider add a prop called 'value' (so any child can extract the 'valie'):
+//value can be= int, 'string'. [arr]- we have a list of blogposts Flatlist)
+return <BlogContext.Provider value={blogPosts}>
     {children}    
 </BlogContext.Provider>
 };
 //{children} is a var - initialized in App.js as <App>
 //it means all wrapped stack navigator (in custom "app" component)
 // is directly accessed by the blogcontext
+
+export default BlogContext;
+//export default it - so other children could access it and the data in it directly
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';//imported hook into the child : useContext - to make use of that contex (from BlogContext)
+//function that's going look at some context object and give us access (to value prop - for example)
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+//need flatlist for  arr of posts (objects)
+import BlogContext from '../context/BlogContext';
+
+
 
 const IndexScreen = () => {
+//variable 'blogPosts' here is going to be exactly = to the value prop assigned in the blogcontext.provider
+const blogPosts = useContext(BlogContext);
+
     return (
         <View>
             <Text>Index Screen</Text>
+             <FlatList
+//data prop =blog posts: our array of objects 
+
+                data={blogPosts}
+
+// key extractor: a function that's going to be called with every object inside of our array.
+// from key extractor we have to return a String to be used as a key: in our case we have a unique string -
+//It's the title of every blog post.
+                keyExtractor={(blogPosts) => blogPosts.title}
+
+//renderItem is called with a argument that is an object that has a couple of different properties on it.
+//The only property we care about is the item property.
+//So we destructure that off with a set of curly braces 
+// item inside is equal to our individual blog post objects:                
+                renderItem={({ item }) => {
+                return <Text>{item.title}</Text>;
+                }}
+             />
         </View>
     );
 };
