48d5650a72e6ee58a71ec3c20d5e972453b08d2a
Natasha S Love <debora@inbox.ru>
2020-01-25 21:16:46 -0600

    updated with useReducer
    adjusted all code to useReducer instead of useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,21 @@
-import React, { useState } from 'react';//adding useState helper function to rerender app with new data
+import React, { useReducer } from 'react';
+//adding useState helper function to rerender app with new data
+//but going to use hook - useReducer instead- the key to the reducer function - 
+// no matter what it always returns a new value to be used as our state object
 
 const BlogContext = React.createContext();
+//reducer has 2 args: 1. state, 2. action and uses switch statement to do the action clicked on:
+const blogReducer = (state, action)=>{
+//the difference with reducer is that we can very easily add additional case statements to switch and can modify our list of blog posts in different ways:
+    switch (action.type) {
+        case 'add_blogpost':
+//similar codes described below in comments from useState
+            return [...state,{ title: `Blog Post #${state.length + 1}`}];//2 arg- action
+        default:
+            return state;
+
+    }
+};
 
 //create blog post provider
 //export in order to make use of it inside of app.js
@@ -9,20 +24,31 @@ const BlogContext = React.createContext();
 export const BlogProvider = ( {children})=> {
 // created component which can accept another component as {children}- argument - it is <App> wrapped in this {children} arg   
 
-const [blogPosts, setBlogPosts] = useState([]);//when application first starts up we have an empty array because by defaultthere won't be any blog posts 
-//this setter completely updates this list blogPosts
-// after the state variable - make a new function called addblogposts.
-//the goal this function is to use our setter to add in a new blog post to our blog post variable array:
-const addBlogPost = () => {
-//to update a state variable -we're always going to call the setter:
-// never going to modify the original value of arr blogPosts, so by adding in setBlogPosts([]) a new array -
-//it creates a brand new array from scratch and not changing the original blog post variable 
-//all the existing blog posts are added into this new Array :
-setBlogPosts([ ...blogPosts, {title:`Blog Post #${blogPosts.length+1}`}]);//1st arg -this code means: create a new array and inside that new Array take all the current blog posts we have and add them into the Arr.
-}//2nd arg -new blog post = new object: to have a property title - but changing blog title with every new post:
-//So going to make the title out of a template string: so use `back ticks` and make the title "blog post #" 
-//and to print out the number of this new blog posts=its index - to use it-  place dollar sign $, curly braces{} and inside - the length of blog posts+1.
+const [blogPosts, dispatch] = useReducer(blogReducer, []);//useReducer func: 1arg-reducer itself created earlier, 2 arg- initial state of obj - empty arr
+//when application first starts up we have an empty array because by default there won't be any blog posts 
 
+//create a new temp helper func 'addblogpost' -I'm gonna pass this helper function down into my value prop -
+//so that my index screen can very easily dispatch in action to add in a new blog post:
+const addBlogPost = () => {
+    dispatch({ type: 'add_blogpost'});
+//anytime someone calls add blog post we're going to dispatch an action object, which is 'add_blogpost' from switch
+};
+
+//setBlogPosts -this setter completely updates this list blogPosts- but since now using hook useReducer - changed to 'dispatch' func,
+//also could change 'blogPost' by 'state'(for reducer) - but not necessary, better to remember that it's a list of posts. 
+/*
+    // after the state variable - make a new function called addblogposts. / deleted it all because don't use useState, but useReducer func.
+    //the goal this function is to use our setter to add in a new blog post to our blog post variable array:
+    const addBlogPost = () => {
+    //to update a state variable -we're always going to call the setter:
+    // never going to modify the original value of arr blogPosts, so by adding in setBlogPosts([]) a new array -
+    //it creates a brand new array from scratch and not changing the original blog post variable 
+    //all the existing blog posts are added into this new Array :
+    setBlogPosts([ ...blogPosts, {title:`Blog Post #${blogPosts.length+1}`}]);//1st arg -this code means: create a new array and inside that new Array take all the current blog posts we have and add them into the Arr.
+    }//2nd arg -new blog post = new object: to have a property title - but changing blog title with every new post:
+    //So going to make the title out of a template string: so use `back ticks` and make the title "blog post #" 
+    //and to print out the number of this new blog posts=its index - to use it-  place dollar sign $, curly braces{} and inside - the length of blog posts+1.
+*/
 //creating arr of blog posts - later will use it as 'value' in blogCobtext.provider:
 /*const blogPosts = [
     {title: 'Blog Post #1'},
@@ -38,8 +64,9 @@ setBlogPosts([ ...blogPosts, {title:`Blog Post #${blogPosts.length+1}`}]);//1st 
 //- in blogcontext.provider add a prop called 'value' (so any child can extract the 'valie'):
 //value can be= int, 'string'. [arr]- we have a list of blogposts Flatlist)
 //value -not just blogPost arr, it should be an object with callback func, which rerenders the app-
-//so we need two sets of curly braces to insert an object with props like data and func: (addBlogPost: addBlogPost = addBlogPost) /key:value /
-return <BlogContext.Provider value={{ data: blogPosts, addBlogPost}}>
+//so we need two sets of curly braces to insert an object with props like data and func: (addBlogPost: addBlogPost = addBlogPost) /key:value /-with reducer-don't need useState and func: addBlogPost /erased from value/
+//because we need to use 'dispatch' func with reducer hook:
+return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
     {children}    
 </BlogContext.Provider>
 };
