214b1dbd7740741663877d843c1c5438b80f3dc2
Natasha S Love <debora@inbox.ru>
2020-01-31 13:53:18 -0600

    created form to input text by the user
    useState - to control textInput;
    form and button to submit the post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,16 +1,48 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext, useState } from 'react';//useState here to control TextInput
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';//TextInput for the user to input text
 import { Context } from '../context/BlogContext';
 
 const CreateScreen = ({ navigation})=> {
+//need two different pieces of state one to handle the title entry and one to handle the content entry:
+    const [title, setTitle]= useState('');
+    const[content, setContent] = useState('');
 
     return (
         <View>
-            <Text>Create Screen</Text>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput 
+                style={styles.input} 
+                value={title} 
+                onChangeText ={(text) => setTitle(text)} 
+            />
+            <Text style={styles.label}>Enter Content:</Text>
+            <TextInput 
+                style={styles.input} 
+                value={content} 
+                onChangeText={(text) => setContent(text)} 
+            />
+            <Button title="Add Blog Post" />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+    input: {
+        fontSize:18,
+        borderWidth: 1,
+        borderColor: 'black',
+        marginHorizontal:5,//mRgins on both sides
+        marginBottom:5,
+        paddingHorizontal:5 //to get a little bit spacing between the text and the border
+
+    },
+    label: {
+        fontSize:20,
+        marginBottom:5,
+        marginHorizontal:5
+    }
+
+});
 
 export default CreateScreen;
